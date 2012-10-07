2cd10a316f40f7dcb0373363e040120947dd64ec
Zachary Potter <zerothindex@gmail.com>
2012-10-07 17:56:10 -0400

    Player turns and basic text output.

--- src/com/teamname/abalone/Abalone.java
+++ src/com/teamname/abalone/Abalone.java
@@ -3,8 +3,29 @@ package com.teamname.abalone;
 public class Abalone {
 
 	public static void main(String[] args) {
-		Board board = new Board();
-		board.printBoard();
+		GameMaster game = new GameMaster();
+
+		game.getBoardCopy().printBoard();
+
+		game.makeMove(new Move(Board.BLACK, Board.NE,
+				new int[][]{ {2,2}, {3,2}, {4,2} }));
+		System.out.println("\n==========\n");
+		game.getBoardCopy().printBoard();
+
+		game.makeMove(new Move(Board.WHITE, Board.SW,
+				new int[][]{ {7,8}, {6,7}, {5,6} }));
+		System.out.println("\n==========\n");
+		game.getBoardCopy().printBoard();
+
+		game.makeMove(new Move(Board.BLACK, Board.NW,
+				new int[][]{ {3,3}, {4,3} }));
+		System.out.println("\n==========\n");
+		game.getBoardCopy().printBoard();
+
+		game.makeMove(new Move(Board.WHITE, Board.SW,
+				new int[][]{ {5,6}, {4,5} }));
+		System.out.println("\n==========\n");
+		game.getBoardCopy().printBoard();
 	}
 
 }
